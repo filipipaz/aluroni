85309245bc0f805de3ff02ef42ef3bf16967f90b
filipipaz <134106322+filipipaz@users.noreply.github.com>
2024-02-16 11:40:25 -0300

    Implementar as rotas

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import "normalize.css"
+import 'normalize.css';
 import './index.css';
 import Cardapio from './pages/Cardapio';
+import Inicio from 'pages/Inicio';
+import Router from './routes.js';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Cardapio />
+    <Router />
   </React.StrictMode>
 );
