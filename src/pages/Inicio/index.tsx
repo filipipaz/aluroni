225db3050db676e08c7b5c9aed0950bd9a783919
filipipaz <134106322+filipipaz@users.noreply.github.com>
2024-02-17 10:53:35 -0300

    Imprentar rota dos Pratos com useParams e refatoração das Tags

--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -2,12 +2,20 @@ import cardapio from 'data/cardapio.json';
 import styles from './Inicio.module.scss';
 import stylesTema from 'styles/Tema.module.scss';
 import nossaCasa from 'assets/nossa_casa.png';
+import { useNavigate } from 'react-router-dom';
+import { Prato } from 'types/Prato';
 
 export default function Inicio() {
   let pratosRecomendados = [...cardapio];
   pratosRecomendados = pratosRecomendados
     .sort(() => 0.5 - Math.random())
     .splice(0, 3);
+
+  const navigate = useNavigate();
+
+  function redirecionarParaDetalhes(prato: Prato) {
+    navigate(`/prato/${prato.id}`, { state: { prato }  });
+  }
   return (
     <section>
       <h3 className={stylesTema.titulo}>Recomendações da cozinha</h3>
@@ -17,13 +25,18 @@ export default function Inicio() {
             <div className={styles.recomendado__imagem}>
               <img src={item.photo} alt={item.title} />
             </div>
-            <button className={styles.recomendado__botao}>Ver mais</button>
+            <button
+              className={styles.recomendado__botao}
+              onClick={() => redirecionarParaDetalhes(item)}
+            >
+              Ver mais
+            </button>
           </div>
         ))}
       </div>
       <h3 className={stylesTema.titulo}> Nossa casa</h3>
       <div className={styles.nossaCasa}>
-        <img src={nossaCasa} alt="Casa do aluroni" />
+        <img src={nossaCasa} alt='Casa do aluroni' />
         <div className={styles.nossaCasa__endereco}>
           Rua Vergueiro, 3185 <br /> <br /> Vila Mariana - SP
         </div>
